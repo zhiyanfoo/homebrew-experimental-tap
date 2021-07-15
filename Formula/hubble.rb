8c637a3b373ead6bffbd5e60a0e0894c0b1aee4e
Zhiyan Foo <zhiyanfoo@gmail.com>
2021-07-15 01:50:22 -0400

    use git

--- Formula/hubble.rb
+++ Formula/hubble.rb
@@ -2,15 +2,16 @@ require 'open3'
 
 class Hubble < Formula
   desc "Hubble - Network, Service & Security Observability for Kubernetes using eBPF"
-  homepage ""
-  url "https://github.com/zhiyanfoo/hubble/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "a1d54fed748d96c166051ad66522c8c43e1982f45208dbcf998e15593ac6dcec"
+  homepage "https://github.com/cilium/hubble"
+  url "https://github.com/cilium/hubble.git",
+      tag:      "v0.8.0"
   license "Apache-2.0"
+  head "https://github.com/cilium/hubble.git"
 
   depends_on "go" => :build
 
   def install
-    system 'make', 'hubble-without-git-info'
+    system 'make', 'hubble'
     bin.install 'hubble'
   end
 
