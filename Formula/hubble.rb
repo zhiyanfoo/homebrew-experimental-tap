d02dda782f9ddefd0c7f28274ca10863e3235ae9
Zhiyan Foo <zhiyanfoo@gmail.com>
2021-07-26 00:33:42 +0800

    use tarred source

--- Formula/hubble.rb
+++ Formula/hubble.rb
@@ -3,10 +3,9 @@ require 'open3'
 class Hubble < Formula
   desc "Hubble - Network, Service & Security Observability for Kubernetes using eBPF"
   homepage "https://github.com/cilium/hubble"
-  url "https://github.com/cilium/hubble.git",
-      tag:      "v0.8.0"
+  url "https://github.com/cilium/hubble/releases/download/v0.8.1/hubble-darwin-arm64.tar.gz"
+  sha256 "b4ef62eebdbb0b692480f3334c14bb01bd5b782c5318bfd089621f96851d5096"
   license "Apache-2.0"
-  head "https://github.com/cilium/hubble.git"
 
   depends_on "go" => :build
 
